e18981a12998453d6beb07d05ecf954ce28d2931
John Smith <johnsmith@JohndeMacBook-Pro.local>
2019-06-12 22:52:22 +0800

    feat(parser): add router parser

---
+++ app/routerParse/RouterFlatter.ts
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+
+import { readFile } from "../util";
+// routers
+export default class RouterFlatter {
+  CONFIG_FILES: Array<string> = [];
+  routerData: any = [];
+  constructor() {}
+  async readVueRouter() {
+    debugger;
+  }
+  parseES6RouterString(){
+      
+  }
+}
---
+++ app/routerParse/index.ts
@@ -0,0 +1,4 @@
+/**
+ * this module parse vue-router config and return a flat router array
+ */
+export {default as RouterFlatter} from "./RouterFlatter" 
